702f21ab1a60a9f56180bcdcb7e9b8bd6ca306ef
anviti06 <srivastavanviti@gmail.com>
2020-08-18 12:03:14 +0530

    Jupyter file added

--- app/home.py
+++ app/home.py
@@ -3,7 +3,7 @@ from .train import spam_filter
 home_bp = Blueprint('home_bp', __name__)
 
 @home_bp.route('/')
-def main(methods = ['POST' , 'GET']):
+def main(methods = ['POST']):
 	return "Welcome to SMS Spam Detection. Kindly Enter Your message on top"
 
 
